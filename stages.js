dd0a28b20f382d290b430e795d3640c30623c6eb
Bhagyashri <bhagyashri.zade912@gmail.com>
2025-11-17 17:23:55 +0530

    stage.js changed

--- stages.js
+++ stages.js
@@ -1,14 +1,6 @@
 // stages.js
 const stages = [
   { number: 1, name: "Introduction" },
-  { number: 2, name: "Tiles" },
-  { number: 3, name: "Player Movement" },
-  { number: 4, name: "Enemies" },
-  { number: 5, name: "Scoring" },
-  { number: 6, name: "Power-ups" },
-  { number: 7, name: "Collision" },
-  { number: 8, name: "Level Design" },
-  { number: 9, name: "Sound Effects" },
-  { number: 10, name: "Menus" },
+
   // ... continue up to 500+ stages
 ];
